5fad9d02fb1116366e53ce8952e9043faeb2c28b
sensokame <justhoucem@gmail.com>
2016-11-13 02:31:30 +0100

    added camera necessity and push notifications

---
+++ app/src/main/java/com/example/houcem/natco/services/FireBaseImplementation.java
@@ -0,0 +1,25 @@
+package com.example.houcem.natco.services;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by houcem on 13/11/16.
+ */
+
+public class FireBaseImplementation extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // TODO: Implement this method to send any registration to your app's servers.
+        //sendRegistrationToServer(refreshedToken);
+    }
+}
